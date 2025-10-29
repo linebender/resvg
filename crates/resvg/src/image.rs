1c0a3efd749d1b4b7d70a029f26694263c33107c
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-10-29 09:38:37 +0100

    Bump to 0.5.5

--- crates/resvg/src/image.rs
+++ crates/resvg/src/image.rs
@@ -95,14 +95,6 @@ mod raster_images {
             let data = decoder.decode().ok()?;
             match output_cs {
                 ColorSpace::RGBA => data,
-                // `set_output_color_space` is not guaranteed to actually always set the output space
-                // to RGBA (its docs say "we do not guarantee the decoder can convert to all colorspaces").
-                // In particular, it seems like it doesn't work for luma JPEGs,
-                // so we convert them manually.
-                ColorSpace::Luma => data
-                    .into_iter()
-                    .flat_map(|p| [p, p, p, 255])
-                    .collect::<Vec<_>>(),
                 _ => return None,
             }
         };
